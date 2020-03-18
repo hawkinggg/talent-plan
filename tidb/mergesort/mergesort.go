e38828df26d5b05be73d3398e6eb6dd4a089c6d3
hawkinggg <690302166@qq.com>
2020-03-18 18:41:50 +0800

    it's ok! golang concurrency merge sort faster than signal thread quick sort

--- tidb/mergesort/mergesort.go
+++ tidb/mergesort/mergesort.go
@@ -1,5 +1,7 @@
 package main
 
+import "sync"
+
 // MergeSort performs the merge sort algorithm.
 // Please supplement this function to accomplish the home work.
 func MergeSort(src []int64) {
@@ -8,26 +10,46 @@ func MergeSort(src []int64) {
 	merge(tmp, src, 0, len(src))
 }
 
+var sem = make(chan struct{}, 100)
+
 func merge(src []int64, dst []int64, startIdx int, endIdx int) {
 	l := endIdx - startIdx
 	if l > 2 {
 		half := startIdx + l/2
-		//var wg sync.WaitGroup
-		//wg.Add(2)
 
-		//go func(src1 []int64, dst1 []int64, startIdx1 int, endIdx1 int) {
-		//	defer wg.Done()
-		//	merge(src1, dst1, startIdx1, endIdx1)
-		//}(dst, src, startIdx, half)
-		//
-		//go func(src2 []int64, dst2 []int64, startIdx2 int, endIdx2 int) {
-		//	defer wg.Done()
-		//	merge(src2, dst2, startIdx2, endIdx2)
-		//}(dst, src, half, endIdx)
+		var wg sync.WaitGroup
 
-		//wg.Wait()
-		merge(dst, src, startIdx, half)
-		merge(dst, src, half, endIdx)
+		select {
+		case sem <- struct{}{}:
+			{
+				wg.Add(1)
+				go func() {
+					defer wg.Done()
+					merge(dst, src, startIdx, half)
+					<-sem
+				}()
+			}
+		default:
+			merge(dst, src, startIdx, half)
+		}
+
+		select {
+		case sem <- struct{}{}:
+			{
+				wg.Add(1)
+				go func() {
+					defer wg.Done()
+					merge(dst, src, half, endIdx)
+					<-sem
+				}()
+			}
+		default:
+			merge(dst, src, half, endIdx)
+		}
+
+		wg.Wait()
+		//merge(dst, src, startIdx, half)
+		//merge(dst, src, half, endIdx)
 
 		idxLeft, idxRight := startIdx, half
 		for i := startIdx; i<endIdx; i++ {
